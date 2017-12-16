b8895e041092c065df3f252bf042b7697b1ce8c6
charles <ctognol@laposte.net>
2017-12-16 11:28:19 +0100

    nouvelle fonction meAjax
    meAjax creer fonction ajax perso multi methode pouvant accepter des
    tableaux de donnée.
    meAjax(string url, string methode, object data, [array] string nomData,
    function callback, object JSON)

--- myLib.js
+++ myLib.js
@@ -1,3 +1,83 @@
+
+//Affiche un message d'erreur passer en argument après avoir créer une div et l'ajoute 
+// à l'element parent passer en argument
+"position: absolute; background-color: black; top: 0px; height: 100%"
+function msgReponse(elmentParent, message, typeMsg){
+    var background;
+    typeMsg ? background = ["rgba(0, 255, 0, 0.2)", '#00806A'] : background = ["rgba(255, 0, 0, 0.2)", '#D8000C'];
+    var divElt = document.createElement('div');
+    divElt.setAttribute("class", "col-12");
+    var styleElmts = {
+        position: "absolute",
+        border: "1px solid #00806A",
+        padding: "1.5%",
+        top: "49%",
+        //left: "0px",
+        textAlign: "center",
+        //width: "100%", attr col-12 car gerer par bootstrap
+        height: "10%",
+        backgroundColor: background[0],
+        color: background[1],
+        fontSize: '25px',
+        borderRadius: '5px'
+    };
+    for(st in styleElmts){
+        divElt.style[st] = styleElmts[st];
+    }
+    divElt.textContent = message;
+    elmentParent.appendChild(divElt);
+    setTimeout(function(){elmentParent.removeChild(divElt)}, 10000);
+}
+
+ function meAjax(url, methode, data, nameData, callback, isJson){
+
+    var xhr = new XMLHttpRequest();
+    var form = new FormData();
+
+    xhr.open(methode, url);
+
+    xhr.addEventListener("load", function () {
+        if (xhr.status >= 200 && xhr.status < 400) {
+            console.log("status de la reuete : " + xhr.status);
+            callback(xhr.responseText);
+        } else {
+            console.error(xhr.status + " " + xhr.statusText + " " + url);
+        }
+    });
+
+    xhr.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+
+    if (isJson) {
+        // Définit le contenu de la requête comme étant du JSON
+        req.setRequestHeader("Content-Type", "application/json");
+        // Transforme la donnée du format JSON vers le format texte avant l'envoi
+        data = JSON.stringify(data);
+    }
+
+    if(Array.isArray(nameData)){
+        var i = 0;
+        nameData.forEach( function(element){
+            form.append(element, data[i]);
+            i++;
+        });    
+    }else if (methode == "POST") {
+        form.append(nameData, data);
+    }else{
+        form = null;
+    }
+    //form.append('action', );
+    xhr.send(form);
+}
+
+function refresh(url, time) {
+    var metaElt = document.createElement('meta');
+    metaElt.setAttribute('http-equiv', 'refresh');
+    metaElt.setAttribute('content', time+'; URL='+url);
+    document.head.appendChild(metaElt);
+}
+
 // Exécute un appel AJAX GET
 // Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
 function ajaxGet(url, callback) {
@@ -42,11 +122,4 @@ function ajaxPost(url, data, callback, isJson) {
         data = JSON.stringify(data);
     }
     req.send(data);
-}
-
-function refresh(url, time) {
-    var metaElt = document.createElement('meta');
-    metaElt.setAttribute('http-equiv', 'refresh');
-    metaElt.setAttribute('content', time+'; URL='+url);
-    document.head.appendChild(metaElt);
 }
